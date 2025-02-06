1d42efd77704f937bcc894d05b726a52aaa569a9
Nikola Hristov <Nikola@PlayForm.LTD>
2025-02-06 07:58:05 +0200

    s
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.LTD>

--- Source/Fn/Binary/Document.rs
+++ Source/Fn/Binary/Document.rs
@@ -4,7 +4,7 @@ use Library::Fn::Cache;
 use clap::{Command, arg};
 
 fn main() {
-	let matches = Command::new("📄 Document.")
+	let matches = Command::new("Document 📄")
 		.version(env!("CARGO_PKG_VERSION"))
 		.author("Nikola Hristov")
 		.about("Build.")
